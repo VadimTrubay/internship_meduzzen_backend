303a9f73152cca38f5b40149440c82f460a9a44f
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-03 15:40:13 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 def test_health_check(client):
-    response = client.get("/")
+    response = client.get("/healthcheck")
     assert response.status_code == 200
     assert response.json()["detail"] == "ok"
     assert response.json()["result"] == "working"
