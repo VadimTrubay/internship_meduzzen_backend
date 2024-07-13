01d83626bd280e11b6bc9e00b86a30e5afcdd78e
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-13 17:09:25 +0300

    finish comit #6

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -21,20 +21,20 @@ async def get_auth_service(session: AsyncSession = Depends(get_session)) -> Auth
 
 @router.post("/login", response_model=TokenModel)
 async def login_jwt(
-        login_data: SignInRequest, auth_service: AuthService = Depends(get_auth_service)
+    login_data: SignInRequest, auth_service: AuthService = Depends(get_auth_service)
 ):
     return await auth_service.validate_auth_user(login_data.model_dump())
 
 
 @router.post("/signup", response_model=TokenModel)
 async def create_user(
-        user_create: SignUpRequest, user_service: AuthService = Depends(get_auth_service)
+    user_create: SignUpRequest, user_service: AuthService = Depends(get_auth_service)
 ):
     return await user_service.create_user(user_create.model_dump())
 
 
 @router.get("/me", response_model=UserSchema)
 async def get_current_user_route(
-        current_user: UserSchema = Depends(AuthService.get_current_user),
+    current_user: UserSchema = Depends(AuthService.get_current_user),
 ):
     return current_user
