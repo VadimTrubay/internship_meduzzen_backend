c4e6fe25af531d23724fb6e3e9409f786be4627b
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-15 14:16:12 +0300

    Update jwt_utils.py

--- app/utils/jwt_utils.py
+++ app/utils/jwt_utils.py
@@ -1,16 +1,17 @@
 from datetime import datetime, timedelta
 import jwt
+from loguru import logger
 
 from app.conf.config import settings
 
 
 async def encode_jwt(
-    payload: dict,
-    client_secret: str = settings.AUTH0_SECRET,
-    algorithm: str = settings.AUTH0_ALGORITHM,
-    audience: str = settings.AUTH0_API_AUDIENCE,
-    expire_minutes: int = settings.TOKEN_EXPIRATION,
-    expire_timedelta: timedelta | None = None,
+        payload: dict,
+        client_secret: str = settings.AUTH0_SECRET,
+        algorithm: str = settings.AUTH0_ALGORITHM,
+        audience: str = settings.AUTH0_API_AUDIENCE,
+        expire_minutes: int = settings.TOKEN_EXPIRATION,
+        expire_timedelta: timedelta | None = None,
 ) -> str:
     to_encode = payload.copy()
     now = datetime.utcnow()
@@ -23,23 +24,36 @@ async def encode_jwt(
         iat=now,
         aud=audience,
     )
-    encoded = jwt.encode(
+    encoded_jwt = jwt.encode(
         to_encode,
         client_secret,
         algorithm=algorithm,
     )
-    return encoded
+    return encoded_jwt
 
 
-def decode_jwt(
-    token: str | bytes,
-    client_secret: str = settings.AUTH0_SECRET,
-    algorithm: str = settings.AUTH0_ALGORITHM,
-) -> dict:
+def decode_jwt_token(token: str, ) -> dict:
     decoded = jwt.decode(
         token,
-        client_secret,
-        algorithms=[algorithm],
+        settings.AUTH0_SECRET,
+        algorithms=[settings.AUTH0_ALGORITHM],
+        audience=settings.API_AUDIENCE,
+    )
+    return decoded
+
+
+def decode_auth0_token(token: str) -> dict:
+    decoded = jwt.decode(
+        token,
+        settings.AUTH0_SECRET,
+        algorithms=[settings.AUTH0_ALGORITHM],
         audience=settings.AUTH0_API_AUDIENCE,
     )
     return decoded
+
+
+def decode_jwt(token: str) -> dict:
+    try:
+        return decode_jwt_token(token)
+    except Exception as e:
+        logger.info(f"Exception occurred own token {e}")
