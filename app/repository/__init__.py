36f5f49fcdc684077b35d2f0ba63b875dc20e7b0
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-22 19:40:55 +0300

    added migretions actions

--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -1,2 +1,4 @@
 from app.models.base_model import BaseModel
 from app.models.user_model import BaseModel
+from app.models.company_model import BaseModel
+from app.models.action_model import BaseModel
