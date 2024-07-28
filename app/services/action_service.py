67923dff907dbc1ff27fb53a479c0c2bb65c9ada
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-28 16:28:02 +0300

    Update action_service.py

--- app/services/action_service.py
+++ app/services/action_service.py
@@ -168,7 +168,7 @@ class ActionService:
 
     # CREATE REQUEST
     async def create_request(
-        self, action_data: RequestCreateSchema, current_user_id: id
+        self, action_data: RequestCreateSchema, current_user_id: uuid.UUID
     ) -> ActionSchema:
         company = await self._get_company_or_raise(action_data.company_id)
         request = await self.action_repository.get_one(
