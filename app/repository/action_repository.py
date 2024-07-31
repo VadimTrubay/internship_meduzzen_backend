b14582cd47495ba78923385f06ef60e2305ec6de
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-31 16:22:52 +0300

    Update action_repository.py

--- app/repository/action_repository.py
+++ app/repository/action_repository.py
@@ -25,10 +25,13 @@ class ActionRepository(BaseRepository):
                 .distinct()
                 .join(User, CompanyAction.user_id == User.id)
                 .join(Company, CompanyAction.company_id == Company.id)
-                .join(CompanyMember, and_(
-                    CompanyAction.company_id == CompanyMember.company_id,
-                    CompanyAction.user_id == CompanyMember.user_id
-                ))
+                .join(
+                    CompanyMember,
+                    and_(
+                        CompanyAction.company_id == CompanyMember.company_id,
+                        CompanyAction.user_id == CompanyMember.user_id,
+                    ),
+                )
                 .filter(id_column == id_, CompanyAction.status == status)
             )
         if not is_company:
@@ -37,9 +40,13 @@ class ActionRepository(BaseRepository):
                 .distinct()
                 .join(User, CompanyAction.user_id == User.id)
                 .join(Company, CompanyAction.company_id == Company.id)
-                .join(CompanyMember, CompanyAction.company_id == CompanyMember.company_id)
+                .join(
+                    CompanyMember, CompanyAction.company_id == CompanyMember.company_id
+                )
                 # .join(CompanyMember, CompanyAction.user_id == CompanyMember.user_id)
                 .filter(id_column == id_, CompanyAction.status == status)
             )
         return query
+
+
 # CompanyAction.user_id == CompanyMember.user_id
