1fc953c9997d88a204fa7785c9fab2098467ecb7
VadimTrubay <vadnetvadnet@ukr.net>
2024-07-31 16:22:46 +0300

    Update action_repository.py

--- app/repository/action_repository.py
+++ app/repository/action_repository.py
@@ -19,18 +19,27 @@ class ActionRepository(BaseRepository):
         id_: uuid.UUID, status: InvitationStatus, is_company: bool
     ):
         id_column = CompanyAction.company_id if is_company else CompanyAction.user_id
-        query = (
-            select(CompanyAction, User, Company, CompanyMember)
-            .distinct()
-            .join(User, CompanyAction.user_id == User.id)
-            .join(Company, CompanyAction.company_id == Company.id)
-            .join(
-                CompanyMember,
-                and_(
+        if is_company:
+            query = (
+                select(CompanyAction, User, Company, CompanyMember)
+                .distinct()
+                .join(User, CompanyAction.user_id == User.id)
+                .join(Company, CompanyAction.company_id == Company.id)
+                .join(CompanyMember, and_(
                     CompanyAction.company_id == CompanyMember.company_id,
-                    # CompanyAction.user_id == CompanyMember.user_id
-                ),
+                    CompanyAction.user_id == CompanyMember.user_id
+                ))
+                .filter(id_column == id_, CompanyAction.status == status)
             )
-            .filter(id_column == id_, CompanyAction.status == status)
-        )
+        if not is_company:
+            query = (
+                select(CompanyAction, User, Company, CompanyMember)
+                .distinct()
+                .join(User, CompanyAction.user_id == User.id)
+                .join(Company, CompanyAction.company_id == Company.id)
+                .join(CompanyMember, CompanyAction.company_id == CompanyMember.company_id)
+                # .join(CompanyMember, CompanyAction.user_id == CompanyMember.user_id)
+                .filter(id_column == id_, CompanyAction.status == status)
+            )
         return query
+# CompanyAction.user_id == CompanyMember.user_id
