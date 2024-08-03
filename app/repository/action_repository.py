0ca3d1f0e447d059758204f308952429797d5496
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-08-03 21:35:28 +0300

    added quizzes

--- app/repository/action_repository.py
+++ app/repository/action_repository.py
@@ -17,12 +17,13 @@ class ActionRepository(BaseRepository):
         super().__init__(session=session, model=CompanyAction)
 
     async def get_members(self, company_id: uuid.UUID) -> List[CompanyMemberSchema]:
+        # query = select(CompanyMember).where(CompanyMember.company_id == company_id)
         query = (
             select(CompanyMember, CompanyAction)
-            .join(CompanyAction, CompanyAction.user_id == CompanyMember.user_id)
-            .join(User, User.id == CompanyAction.user_id)
-            .join(Company, Company.id == CompanyAction.company_id)
-            .filter(CompanyMember.company_id == company_id)
+            .join(CompanyAction, CompanyAction.company_id == CompanyMember.company_id)
+            # .join(User, User.id == CompanyAction.user_id)
+            # .join(Company, Company.id == CompanyAction.company_id)
+            .filter(CompanyAction.company_id == company_id)
         )
         result = await self.session.execute(query)
         return result.all()
@@ -37,7 +38,7 @@ class ActionRepository(BaseRepository):
 
     @staticmethod
     async def get_relatives_query(
-            id_: uuid.UUID, status: InvitationStatus, is_company: bool
+        id_: uuid.UUID, status: InvitationStatus, is_company: bool
     ):
         id_column = CompanyAction.company_id if is_company else CompanyAction.user_id
 
---
+++ app/routers/quizzes.py
@@ -0,0 +1,108 @@
+import uuid
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.connection import get_session
+from app.repository.action_repository import ActionRepository
+from app.repository.company_repository import CompanyRepository
+from app.repository.quizzes_repository import QuizRepository
+from app.schemas.quizzes import (
+    QuizSchema,
+    QuizUpdateSchema,
+    QuestionSchema,
+    QuizResponseSchema,
+)
+from app.schemas.users import UserSchema
+from app.services.auth_service import AuthService
+from app.services.quiz_service import QuizService
+
+router = APIRouter(prefix="/quizzes", tags=["quizzes"])
+
+
+async def get_quizzes_service(
+    session: AsyncSession = Depends(get_session),
+) -> QuizService:
+    action_repository = ActionRepository(session)
+    company_repository = CompanyRepository(session)
+    quiz_repository = QuizRepository(session)
+    return QuizService(
+        session=session,
+        quiz_repository=quiz_repository,
+        action_repository=action_repository,
+        company_repository=company_repository,
+    )
+
+
+@router.get("/{company_id}", response_model=List[QuizResponseSchema])
+async def get_quizzes(
+    company_id: uuid.UUID,
+    current_user: UserSchema = Depends(AuthService.get_current_user),
+    quiz_service: QuizService = Depends(get_quizzes_service),
+) -> List[QuizResponseSchema]:
+    current_user_id = current_user.id
+    return await quiz_service.get_quizzes(company_id)
+
+
+@router.post("/{company_id}/create", response_model=QuizSchema)
+async def create_quiz(
+    quiz_data: QuizSchema,
+    company_id: uuid.UUID,
+    current_user: UserSchema = Depends(AuthService.get_current_user),
+    quiz_service: QuizService = Depends(get_quizzes_service),
+) -> QuizSchema:
+    current_user_id = current_user.id
+    return await quiz_service.create_quiz(
+        quiz_data=quiz_data, current_user_id=current_user_id, company_id=company_id
+    )
+
+
+@router.patch("/{quiz_id}/update", response_model=QuizUpdateSchema)
+async def update_quiz(
+    quiz_data: QuizUpdateSchema,
+    quiz_id: uuid.UUID,
+    current_user: UserSchema = Depends(AuthService.get_current_user),
+    quiz_service: QuizService = Depends(get_quizzes_service),
+) -> QuizUpdateSchema:
+    current_user_id = current_user.id
+    return await quiz_service.update_quiz(
+        quiz_data=quiz_data, quiz_id=quiz_id, current_user_id=current_user_id
+    )
+
+
+@router.delete("/{quiz_id}/delete", response_model=QuizResponseSchema)
+async def delete_quiz(
+    quiz_id: uuid.UUID,
+    current_user: UserSchema = Depends(AuthService.get_current_user),
+    quiz_service: QuizService = Depends(get_quizzes_service),
+) -> QuizResponseSchema:
+    current_user_id = current_user.id
+    return await quiz_service.delete_quiz(
+        quiz_id=quiz_id, current_user_id=current_user_id
+    )
+
+
+@router.post("/{quiz_id}/question/create", response_model=QuestionSchema)
+async def create_question(
+    question_data: QuestionSchema,
+    quiz_id: uuid.UUID,
+    current_user: UserSchema = Depends(AuthService.get_current_user),
+    quiz_service: QuizService = Depends(get_quizzes_service),
+) -> QuestionSchema:
+    current_user_id = current_user.id
+    return await quiz_service.add_question(
+        question_data, quiz_id, current_user_id=current_user_id
+    )
+
+
+@router.delete("/question/{question_id}/delete", response_model=QuestionSchema)
+async def delete_quiz_question(
+    question_id: uuid.UUID,
+    current_user: UserSchema = Depends(AuthService.get_current_user),
+    quiz_service: QuizService = Depends(get_quizzes_service),
+) -> QuestionSchema:
+    current_user_id = current_user.id
+    return await quiz_service.delete_question(
+        question_id, current_user_id=current_user_id
+    )
