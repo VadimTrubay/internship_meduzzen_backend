36d15a03e2ad9ad7634f484e13e96cd2652f6916
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-08-11 10:54:01 +0300

    Added redis result

--- app/db/redis_db.py
+++ app/db/redis_db.py
@@ -1,10 +1,15 @@
-import asyncio_redis
+import redis.asyncio as redis
 from app.conf.config import settings
+
+
+redis_connection = redis.from_url(
+    f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}", decode_responses=True
+)
 
 
 async def check_redis_connection():
     try:
-        connection = await asyncio_redis.Connection.create(
+        connection = await redis.Connection.create(
             host=settings.redis_host, port=settings.redis_port
         )
         await connection.ping()
