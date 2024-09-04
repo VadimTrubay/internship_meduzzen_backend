90404542eb9a60ac29708cb4ea72aa59346d981a
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-09-04 07:37:21 +0300

    refactored

--- app/routers/users.py
+++ app/routers/users.py
@@ -33,6 +33,7 @@ async def get_all_users(
     skip: int = 1,
     limit: int = 10,
     user_service=Depends(get_user_service),
+    current_user: UserSchema = Depends(AuthService.get_current_user),
 ):
     users = await user_service.get_users(skip, limit)
     total_count = await user_service.get_total_count()
